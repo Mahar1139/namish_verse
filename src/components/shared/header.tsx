6954c3e9b97a43f020805f1d2c87aaffba615ee4
Ashish Mahar <asmaharg1139@gmail.com>
2025-12-09 08:26:01 +0000

    add some space in left  (_for element span_)

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -22,7 +22,7 @@ export function SiteHeader() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 items-center">
-        <Link href="/" className="mr-auto flex items-center space-x-2">
+        <Link href="/" className="mr-auto flex items-center space-x-2 ml-4">
           <span className="font-bold font-headline">Namish Verse</span>
         </Link>
 
