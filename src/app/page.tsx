49880eefca854c9afb273e43421bc1a87ff80b2b
Ashish Mahar <asmaharg1139@gmail.com>
2025-12-13 05:37:22 +0000

    i have created the folder but it is undefined so what can i do

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,24 @@
 import { Button } from '@/components/ui/button';
-import { PlaceHolderImages } from '@/lib/placeholder-images';
 import { MoveRight } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Home() {
-  const homeHeroImage = PlaceHolderImages.find(img => img.id === 'home_hero');
   return (
     <section className="relative h-dvh w-full overflow-hidden">
       <div className="absolute inset-0 bg-light-motion z-0"></div>
       <div className="relative z-10 flex h-full flex-col items-center justify-center text-center px-4">
         
         <div className="animate-subtle-float space-y-8">
-            {homeHeroImage && (
-                <div className="relative mx-auto w-40 h-40 md:w-48 md:h-48 rounded-full overflow-hidden shadow-2xl shadow-primary/20 border-4 border-primary/20">
-                    <Image
-                    src={homeHeroImage.imageUrl}
-                    alt={homeHeroImage.description}
-                    data-ai-hint={homeHeroImage.imageHint}
-                    fill
-                    className="object-cover"
-                    />
-                </div>
-            )}
+            <div className="relative mx-auto w-40 h-40 md:w-48 md:h-48 rounded-full overflow-hidden shadow-2xl shadow-primary/20 border-4 border-primary/20">
+                <Image
+                src="/nammu_pics/pf_p.jpg"
+                alt="Portrait of Namish Gohil"
+                data-ai-hint="male dancer"
+                fill
+                className="object-cover"
+                />
+            </div>
           <div>
             <h1 className="font-headline text-5xl md:text-7xl lg:text-8xl font-bold tracking-tighter glowing-text">
               Namish Gohil
